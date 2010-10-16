65a272ff6eb4536ee0a482f87a14bc2f2e0d0d51
Alistair Stead <alistair.stead@designdisclosure.com>
2010-10-16 01:01:19 +0100

    work in progress for the extension generation

--- MageTool/Tool/MageExtension/Provider/Abstract.php
+++ MageTool/Tool/MageExtension/Provider/Abstract.php
@@ -12,16 +12,44 @@ require_once 'Zend/Tool/Framework/Provider/Interface.php';
  * @package default
  * @author Alistair Stead
  **/
-abstract class MageTool_Tool_MageExtension_Provider_Abstract implements Zend_Tool_Framework_Provider_Interface
+abstract class MageTool_Tool_MageExtension_Provider_Abstract extends Zend_Tool_Project_Provider_Abstract
 {
     /**
-     * Overload the constructor to add the setup for finding the mage file
+     * undocumented function
      *
      * @return void
      * @author Alistair Stead
      **/
-    public function __construct()
+    protected function _bootstrap()
     {
-        // TODO bootstrap the Magento application
+        //load Magento
+        $mageFilename = 'app/Mage.php';
+        $this->_isInstalled($mageFilename);
+    }
+    
+    /**
+     * Find the mage file and confirm Magento is installed
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    protected function _isInstalled($mageFilename)
+    {
+        if (!file_exists($mageFilename)) {
+            throw new MageTool_Tool_Provider_Exception('The mage.php file can not be located. You must run this command within a Magento project.');
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Build the current directory path
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    protected function _chApplicationDir()
+    {
+        chdir(getcwd());
     }
 }
--- MageTool/Tool/MageExtension/Provider/Extension.php
+++ MageTool/Tool/MageExtension/Provider/Extension.php
@@ -1,10 +1,10 @@
 <?php
-
+require_once 'MageTool/Tool/MageExtension/Profile.php';
 /**
  * @see MageTool_Tool_MageExtension_Provider_Abstract
  */
 require_once 'MageTool/Tool/MageExtension/Provider/Abstract.php';
-
+require_once 'MageTool/Tool/MageExtension/Provider/Exception.php';
 /**
  * undocumented class
  *
@@ -31,8 +31,109 @@ class MageTool_Tool_MageExtension_Provider_Extension extends MageTool_Tool_MageE
      * @return void
      * @author Alistair Stead
      **/
-    public function create($codePool, $vendorName, $extensionName)
+    public function create($vendor, $name, $pool = 'local', $fileOfProfile = null)
     {
-        echo "Create extension";
+        $this->_bootstrap();
+        $this->_chApplicationDir();
+        $path = getcwd();
+        
+        $path = sprintf("%s/app/code/%s/%s/%s", $path, $pool, $vendor, $name);
+
+        if (file_exists($path)) {
+            throw new MageTool_Tool_MageExtension_Provider_Exception(
+                "An Extension {$extensionName} already exists in the {$codePool} code pool for the vendor {$vendorName}."
+                );
+        } else {
+            try {
+                mkdir($path, 0755, true);
+            } catch (Exception $e) {
+                throw new MageTool_Tool_MageExtension_Provider_Exception(
+                    "Unable to create Extension {$extensionName} directory."
+                    );
+            }
+        }
+
+        $profileData = null;
+
+        if ($fileOfProfile != null && file_exists($fileOfProfile)) {
+            $profileData = file_get_contents($fileOfProfile);
+        }
+
+        if ($profileData == '') {
+            $profileData = $this->_getDefaultProfile();
+        }
+
+        $newProfile = new MageTool_Tool_MageExtension_Profile(array(
+            'projectDirectory' => $path,
+            'profileData' => $profileData
+            ));
+
+        $newProfile->loadFromData();
+
+        $response = $this->_registry->getResponse();
+        
+        $response->appendContent('Creating extension at ' . $path);
+        $response->appendContent('Note: ', array('separator' => false, 'color' => 'yellow'));
+        $response->appendContent(
+            'This command created a new extension, '
+            . 'you will now need to create a config file to enable this module in app/etc/modules');
+
+        foreach ($newProfile->getIterator() as $resource) {
+            $resource->create();
+        }
+    }
+    
+    protected function _getDefaultProfile()
+    {
+        $data = <<<EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
+<extensionProfile type="default" version="1.10">
+    <extensionDirectory>
+        <BlockDirectory></BlockDirectory>
+        <ControllerDirectory></ControllerDirectory>
+        <controllersDirectory></controllersDirectory>
+        <etcDirectory>
+            <file filesystemName="config.xaml" defaultContentCallback="MageTool_Tool_MageExtension_Provider_Extension::getDefaultConfigContents"/>
+            <file filesystemName="system.xaml" defaultContentCallback="MageTool_Tool_MageExtension_Provider_Extension::getDefaultSystemContents"/>
+        </etcDirectory>
+        <HelperDiroectory></HelperDiroectory>
+        <ModelDirectory></ModelDirectory>
+        <sqlDirectory></sqlDirectory>
+    </extensionDirectory>
+</extensionProfile>
+EOS;
+        return $data;
+    }
+    
+    public static function getDefaultConfigContents($caller = null)
+    {
+        $projectDirResource = $caller->getResource()->getProfile()->search('projectDirectory');
+        
+        return <<< EOS
+<?xml version="1.0"?>
+<config>
+    <modules>
+        <{$vendor}_{$name}>
+             <version>0.0.1</version>
+        </{$vendor}_{$name}>
+    </modules>
+
+    <global>
+        <models>
+        </models>
+        <blocks>
+            <{$name}>
+            </{$name}>
+        </blocks>
+        <helpers>
+            <{$name}>
+            </{$name}>
+        </helpers>
+    </global>
+    <default>
+    </default>
+</config>
+EOS;
     }
 }
