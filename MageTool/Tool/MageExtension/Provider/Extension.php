eee66f043358918f44381fabf3d5b5eca591e8d9
Alistair Stead <astead@ibuildings.com>
2010-12-01 19:13:35 +0000

    updated extension provider to allow the creation of a default extension based on an XML profile or to pass an alternate profile during generation

--- MageTool/Tool/MageExtension/Provider/Extension.php
+++ MageTool/Tool/MageExtension/Provider/Extension.php
@@ -29,6 +29,13 @@ class MageTool_Tool_MageExtension_Provider_Extension extends MageTool_Tool_MageE
     protected $name;
     
     /**
+     * The code pool into which the module should be placed
+     *
+     * @var string
+     **/
+    protected $pool;
+    
+    /**
      * Define the name of the provider
      *
      * @return string
@@ -51,21 +58,22 @@ class MageTool_Tool_MageExtension_Provider_Extension extends MageTool_Tool_MageE
         $this->_chApplicationDir();
         $path = getcwd();
         
-        $this->vendor = $vendor;
-        $this->name = $name;
+        $this->vendor = ucfirst($vendor);
+        $this->name = ucfirst($name);
+        $this->pool = strtolower($pool);
         
-        $path = sprintf("%s/app/code/%s/%s/%s", $path, $pool, $vendor, $name);
+        $path = sprintf("%s/app/code/%s/%s/%s", $path, $this->pool, $this->vendor, $this->name);
 
         if (file_exists($path)) {
             throw new MageTool_Tool_MageExtension_Provider_Exception(
-                "An Extension {$extensionName} already exists in the {$codePool} code pool for the vendor {$vendorName}."
+                "An Extension {$this->name} already exists in the {$this->pool} code pool for the vendor {$this->vendor}."
                 );
         } else {
             try {
                 mkdir($path, 0755, true);
             } catch (Exception $e) {
                 throw new MageTool_Tool_MageExtension_Provider_Exception(
-                    "Unable to create Extension {$extensionName} directory."
+                    "Unable to create Extension {$this->name} directory."
                     );
             }
         }
@@ -82,18 +90,36 @@ class MageTool_Tool_MageExtension_Provider_Extension extends MageTool_Tool_MageE
 
         $newProfile = new MageTool_Tool_MageExtension_Profile(array(
             'projectDirectory' => $path,
-            'profileData' => $profileData
+            'profileData' => $profileData,
+            'vendor' => $this->vendor,
+            'name' => $this->name,
+            'pool' => $this->pool
             ));
 
         $newProfile->loadFromData();
 
         $response = $this->_registry->getResponse();
         
-        $response->appendContent('Creating extension at ' . $path);
-        $response->appendContent('Note: ', array('separator' => false, 'color' => 'yellow'));
+        $response->appendContent('Created extension at ' . $path, array('color' => 'green'));
+        $response->appendContent('Note: ', array('separator' => true, 'color' => 'yellow'));
         $response->appendContent(
             'This command created a new extension, '
             . 'you will now need to create a config file to enable this module in app/etc/modules');
+        $response->appendContent('Example: ', array('separator' => true, 'color' => 'yellow'));
+        $xmlExample = <<< EOS
+<?xml version="1.0"?>
+<config>
+    <modules>
+        <{$this->vendor}_{$this->name}>
+             <active>true</active>
+             <codePool>{$this->pool}</codePool>
+             <depends>
+             </depends>
+        </{$this->vendor}_{$this->name}>
+    </modules>
+</config>
+EOS;
+        $response->appendContent($xmlExample);
 
         foreach ($newProfile->getIterator() as $resource) {
             $resource->create();
@@ -105,82 +131,35 @@ class MageTool_Tool_MageExtension_Provider_Extension extends MageTool_Tool_MageE
         $data = <<<EOS
 <?xml version="1.0" encoding="UTF-8"?>
 <extensionProfile type="default" version="0.1">
-    <moduleDirectory moduleName="{$this->name}">
+    <ExtensionDirectory>
         <BlockDirectory></BlockDirectory>
         <ControllerDirectory></ControllerDirectory>
         <ControllersDirectory></ControllersDirectory>
         <etcDirectory>
-            <file filesystemName="config.xaml" defaultContentCallback="MageTool_Tool_MageExtension_Provider_Extension::getDefaultConfigContents"/>
-            <file filesystemName="system.xaml" defaultContentCallback="MageTool_Tool_MageExtension_Provider_Extension::getDefaultSystemContents"/>
+            <ConfigFile/>
+            <SystemFile/>
+            <AdminhtmlFile/>
+            <ApiFile/>
+            <WsdlFile/>
         </etcDirectory>
-        <HelperDirectory></HelperDirectory>
-        <ModelDirectory></ModelDirectory>
-        <sqlDirectory></sqlDirectory>
-    </moduleDirectory>
+        <HelperDirectory>
+            <HelperFile name="Data"/>
+        </HelperDirectory>
+        <ModelDirectory>
+            <ModelFile name="{$this->name}"/>
+            <ObserverFile name="Observer"/>
+            <EntityDirectory>
+                <SetupFile/>
+            </EntityDirectory>
+        </ModelDirectory>
+        <sqlDirectory>
+            <SetupDirectory>
+                <InstallFile version="0.1.0"/>
+            </SetupDirectory>
+        </sqlDirectory>
+    </ExtensionDirectory>
 </extensionProfile>
 EOS;
         return $data;
-    }
-    
-    public static function getDefaultConfigContents($caller = null)
-    {
-        $projectDirResource = $caller->getResource()->getProfile()->search('projectDirectory');
-        
-        return <<< EOS
-<?xml version="1.0"?>
-<config>
-    <modules>
-        <{$this->vendor}_{$this->name}>
-             <version>0.0.1</version>
-        </{$this->vendor}_{$this->name}>
-    </modules>
-
-    <global>
-        <models>
-        </models>
-        <blocks>
-            <{$this->name}>
-            </{$this->name}>
-        </blocks>
-        <helpers>
-            <{$this->name}>
-            </{$this->name}>
-        </helpers>
-    </global>
-    <default>
-    </default>
-</config>
-EOS;
-    }
-    
-    public static function getDefaultSystemContents($caller = null)
-    {
-        $projectDirResource = $caller->getResource()->getProfile()->search('projectDirectory');
-        
-        return <<< EOS
-<?xml version="1.0"?>
-<config>
-    <modules>
-        <{$this->vendor}_{$this->name}>
-             <version>0.0.1</version>
-        </{$this->vendor}_{$this->name}>
-    </modules>
-
-    <global>
-        <models>
-        </models>
-        <blocks>
-            <{$this->name}>
-            </{$this->name}>
-        </blocks>
-        <helpers>
-            <{$this->name}>
-            </{$this->name}>
-        </helpers>
-    </global>
-    <default>
-    </default>
-</config>
-EOS;
     }
 }
