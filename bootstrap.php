48909d4d539ef8e691900b38b7b3e2583856107a
Alistair Stead <alistair.stead@designdisclosure.com>
2011-01-23 16:09:13 +0000

    corrected case of included file

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,6 @@
 // Setup the include path for testing
 set_include_path(dirname(__FILE__) . ':' . get_include_path());
 // Include Mage as a dependency for testing
-include_once '/mnt/hgfs/Sites/magento.development.local/public/app/mage.php';
+include_once '/mnt/hgfs/Sites/magento.development.local/public/app/Mage.php';
 
 // require_once 'PHPUnit/Framework.php';
